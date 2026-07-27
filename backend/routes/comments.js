8174c45ebf518feb45ce5ca752d56b84263447f8
afonsoDomingos <karinganastudio23@gmail.com>
2026-07-27 15:00:56 +0200

    fix(comments): fix middleware import path for Render

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getComments, addComment, deleteComment, toggleCommentLike } = require('../controllers/commentController');
-const { protect } = require('../middleware/auth');
+const { protect } = require('../middlewares/auth');
 
 router.route('/:auctionId')
   .get(getComments)
